607663c6c8b619ed5a02f3a44c5d8e764d2ed1e3
hyobin77 <gyqls0509@kumoh.ac.kr>
2025-10-16 13:41:45 +0900

    Hash Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { BrowserRouter } from 'react-router-dom'
+//import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>  
-    <BrowserRouter>
+  <StrictMode>
+    <HashRouter>
       <QueryClientProvider client={queryClient}>
-        <App/>
+        <App />
       </QueryClientProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
